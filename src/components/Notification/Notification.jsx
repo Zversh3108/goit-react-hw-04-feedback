45d0491c7dc5bf35e1a2014cac75888aa37df33c
Zversh3108 <zvershzt3108@gmail.com>
2023-06-29 18:28:56 +0200

    dubug

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 export default function Notification(props) {
   return <p>{props.message}</p>;
